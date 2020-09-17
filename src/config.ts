d862645a51d54684f3a83fab4dee307241aec899
Fabian Bäumer <fabian.baeumer@rub.de>
2020-09-17 12:34:13 +0200

    feat: Total rework of doom

---
+++ src/config.ts
@@ -0,0 +1,42 @@
+import convict from 'convict';
+
+const config = convict({
+  imap: {
+    user: {
+      doc: 'IMAP user',
+      format: String,
+      env: 'SCILABS_IMAP_USER',
+      default: ''
+    },
+    password: {
+      doc: 'IMAP password',
+      format: String,
+      env: 'SCILABS_IMAP_PASSWORD',
+      default: '',
+      sensitive: true
+    },
+    host: {
+      doc: 'IMAP host',
+      format: String,
+      env: 'SCILABS_IMAP_HOST',
+      default: ''
+    },
+    port: {
+      doc: 'IMAP port',
+      format: 'port',
+      env: 'SCILABS_IMAP_PORT',
+      default: 993
+    },
+    tls: {
+      doc: 'IMAP TLS active',
+      format: Boolean,
+      env: 'SCILABS_IMAP_TLS',
+      default: true
+    }
+  }
+});
+
+config.loadFile(__dirname + '/development.json');
+config.validate({ allowed: 'strict' });
+
+export default config;
